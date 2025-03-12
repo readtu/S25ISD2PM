4463f8709d753c4dba3022b8143668e194c13288
jbrown1777 <joshua_brown@taylor.edu>
2025-03-12 14:18:56 -0400

    settings.py was modified and now we can see the html pages on the server

--- CourseCompass/my_app/settings.py
+++ CourseCompass/my_app/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, "Page HTML's")
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
