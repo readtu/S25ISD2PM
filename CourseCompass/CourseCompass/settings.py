95c3da3aa93d0f22abf939f061dd9698bbca4f83
jbrown1777 <joshua_brown@taylor.edu>
2025-03-12 14:15:57 -0400

    Moved files to my_app

--- CourseCompass/CourseCompass/settings.py
+++ CourseCompass/CourseCompass/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, "Page HTML's")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CourseCompass.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- CourseCompass/CourseCompass/views.py
+++ CourseCompass/CourseCompass/views.py
@@ -1,6 +1,9 @@
-from django.hppt import HttpResponse
+from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
-def my_view(request):
-	template = loader.get_template('home_page.html')
-	return HttpResonse(template.render(request))
+#def my_view(request):
+def home_page_view(request):
+	return render(request, "./home_page.html")
+	#template = loader.get_template('home_page.html')
+	#return HttpResonse(template.render(request))
