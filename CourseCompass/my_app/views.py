ef2323242b83fefaaed621ea65ddca8d57b5825a
jbrown1777 <joshua_brown@taylor.edu>
2025-03-12 14:12:36 -0400

    Moved the html pages to the my_app directory

--- CourseCompass/my_app/views.py
+++ CourseCompass/my_app/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpRepsonse
+from django.template import loader
 from django.shortcuts import render
 
+der home_page_view(request):
+	return render(request, "templates/home_page.html")
 # Create your views here.
