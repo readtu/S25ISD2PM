19d56b8514122b4fb8d46182cfed00949510fbc2
jbrown1777 <joshua_brown@taylor.edu>
2025-03-17 14:17:59 -0400

    Added views.py to my_app

--- CourseCompass/my_app/views.py
+++ CourseCompass/my_app/views.py
@@ -2,6 +2,6 @@ from django.http import HttpRepsonse
 from django.template import loader
 from django.shortcuts import render
 
-der home_page_view(request):
+def home_page_view(request):
 	return render(request, "templates/home_page.html")
 # Create your views here.
