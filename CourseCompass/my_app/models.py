d86b244b3bd625200d37c48516c0e325f4680452
dhockett1 <damonhockett22@gmail.com>
2025-03-19 14:45:21 -0400

    Login Forms(Need Manage.py)

--- CourseCompass/my_app/models.py
+++ CourseCompass/my_app/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your models here.
 class Member(models.Model):
   email = models.CharField(max_length=255)
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+  
