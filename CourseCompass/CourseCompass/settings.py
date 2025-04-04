5b632c47858da9ad6426cd55dcb72ccd161b2173
brawlings00 <brianna_rawlings@taylor.edu>
2025-04-04 16:32:20 -0400

    up

--- CourseCompass/CourseCompass/settings.py
+++ CourseCompass/CourseCompass/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static/css',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
