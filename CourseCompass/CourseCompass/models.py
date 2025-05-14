8cebe58120f05b40ff6d0dbc762f896fd56576d0
abeavers3 <andrew_beaverson@taylor.edu>
2025-05-14 16:16:58 -0400

    .py fixes

--- CourseCompass/CourseCompass/models.py
+++ CourseCompass/CourseCompass/models.py
@@ -11,3 +11,9 @@ class LoginForm(AuthenticationForm):
 
 class ModelData(models.Model):
 	json_data = models.JSONField()
+
+class iChairData(models.Model):
+    termCode = models.CharField(max_length=100)
+    termNum = models.CharField(max_length=100)
+    crn = models.CharField(max_length=100)
+
